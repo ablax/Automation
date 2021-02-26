4a21fcef0212b41b70f46356e42e880bb067beda
Ablax <ablax@ablax.me>
2021-02-26 10:38:17 +0200

    Add setters

--- src/test/java/sabev/test/test/pages/TextBoxPage.java
+++ src/test/java/sabev/test/test/pages/TextBoxPage.java
@@ -15,10 +15,38 @@ public class TextBoxPage extends ElementsPage {
     @FindBy(id = "userEmail")
     private WebElement emailField;
 
+    @FindBy(id = "currentAddress")
+    private WebElement currentAddressField;
+
+    @FindBy(id = "permanentAddress")
+    private WebElement permanentAddressField;
+
+    @FindBy(id = "submit")
+    private WebElement submitBtn;
+
     public TextBoxPage(final WebDriver webDriver) {
         super(webDriver);
         PageFactory.initElements(webDriver,this);
         this.webDriver = webDriver;
     }
 
+    public TextBoxPage setUsername(final String username) {
+        this.usernameField.sendKeys(username);
+        return this;
+    }
+
+    public TextBoxPage setEmail(final String email) {
+        this.emailField.sendKeys(email);
+        return this;
+    }
+
+    public TextBoxPage setCurrentAddress(final String currentAddress) {
+        this.currentAddressField.sendKeys(currentAddress);
+        return this;
+    }
+
+    public TextBoxPage setPermanentAddress(final String permanentAddress) {
+        this.permanentAddressField .sendKeys(permanentAddress);
+        return this;
+    }
 }
